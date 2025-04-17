ce5259a3261babd458ca531dcf2ab863c36a9836
Coding rev <fernando.lobo.prez@gmail.com>
2025-04-17 17:53:21 -0300

    05-Controllers
    05-Controllers

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+
+
+const authController = {
+    async register(req,res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+}
+
+module.exports = authController;
