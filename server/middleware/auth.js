db9450ced14cb7d7cf2881e631ff7bc4ec4a6020
Coding rev <fernando.lobo.prez@gmail.com>
2025-04-20 01:46:06 -0300

    17-Checking roles
    17-Checking roles

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,19 +1,39 @@
 const passport = require('passport');
 const { ApiError } = require('./apiError');
 const { status } = require('http-status');
+const { roles } = require('../config/roles')
 
-const verify = (req,res,resolve,reject) => async(err,user)=> {
+const verify = (req,res,resolve,reject,rights) => async(err,user)=> {
     if(err || !user){
         return reject(new ApiError(status.UNAUTHORIZED,'Sorry, unauthorized'))
     }
-    req.user = user;
+    req.user = {
+        _id:user._id,
+        email:user.email,
+        role:user.role,
+        firstname:user.firstname,
+        lastname:user.lastname,
+        age:user.age,
+        verified:user.verified
+    }
+
+
+    if(rights.length){
+        const action = rights[0] /// createAny, readAny ...
+        const resource = rights[1] // profile, articles, test
+        const permission = roles.can(req.user.role)[action](resource);
+        if(!permission.granted){
+            return reject(new ApiError(status.FORBIDDEN,"Sorry, you don't have enough rights"))
+        }
+        res.locals.permission = permission;
+    }
     resolve();
 }
 
 
-const auth = () => async(req,res,next) => {
+const auth = (...rights) => async(req,res,next) => {
     return new Promise((resolve,reject)=>{
-        passport.authenticate('jwt',{session:false},verify(req,res,resolve,reject))(req,res,next)
+        passport.authenticate('jwt',{session:false},verify(req,res,resolve,reject,rights))(req,res,next)
     })
     .then(()=>next())
     .catch((err)=>next(err));
