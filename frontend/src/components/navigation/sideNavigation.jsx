e42c618ff05a0485c7390393b25d02926b32352f
Coding rev <fernando.lobo.prez@gmail.com>
2025-04-23 03:05:09 -0300

    40-Sidenavigation
    40-Sidenavigation

--- frontend/src/components/navigation/sideNavigation.jsx
+++ frontend/src/components/navigation/sideNavigation.jsx
@@ -1,8 +1,91 @@
+import { useState } from "react";
+import { Link as RouterLink } from 'react-router';
+
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+import Divider from '@mui/material/Divider'
+
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+
+import DehazeIcon from '@mui/icons-material/Dehaze'
+import MailIcon from '@mui/icons-material/Mail'
+import HomeIcon from '@mui/icons-material/Home'
+import VpnKeyIcon from '@mui/icons-material/VpnKey'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+
 
 const SideNavigation = () => {
+    const [state,setState] = useState(false)
+
     return(
         <>
-            SideNavigation
+            <DehazeIcon className="drawer_btn" onClick={()=> setState(true)}/>
+            <Drawer anchor={"right"} open={state} onClose={()=>setState(false)}>
+                <Box sx={{width:200}}>
+                    <List>
+
+                        <ListItemButton
+                           component={RouterLink}
+                           to="/"
+                           onClick={()=>setState(false)}
+                        >
+                            <ListItemIcon>
+                                <HomeIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Home"/>
+                        </ListItemButton>
+
+                        <ListItemButton
+                           component={RouterLink}
+                           to="/contact"
+                           onClick={()=>setState(false)}
+                        >
+                            <ListItemIcon>
+                                <MailIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Contact"/>
+                        </ListItemButton>
+
+                        <ListItemButton
+                           component={RouterLink}
+                           to="/auth"
+                           onClick={()=>setState(false)}
+                        >
+                            <ListItemIcon>
+                                <VpnKeyIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Signin"/>
+                        </ListItemButton>
+
+                        <ListItemButton
+                           onClick={()=>alert('Sign out')}
+                        >
+                            <ListItemIcon>
+                                <VpnKeyIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Sign out"/>
+                        </ListItemButton>
+
+                        <>
+                            <Divider/>
+                            <ListItemButton
+                                component={RouterLink}
+                                to="/dashboard"
+                                onClick={()=>setState(false)}
+                            >
+                                <ListItemIcon>
+                                    <DashboardIcon/>
+                                </ListItemIcon>
+                                <ListItemText primary="Dashboard"/>
+                            </ListItemButton>
+                        </>
+
+                    </List>
+                </Box>
+            </Drawer>
         </>
     )
 }
