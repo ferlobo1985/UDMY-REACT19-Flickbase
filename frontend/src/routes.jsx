47af231a597f6a92b61f8da6770388364f80a894
Coding rev <fernando.lobo.prez@gmail.com>
2025-04-23 23:00:58 -0300

    50-Auto sign in part 2
    50-Auto sign in part 2

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router' 
+import { useDispatch, useSelector } from 'react-redux';
+import { isAuth } from './store/actions/users';
+import { Loader } from './utils/tools';
 
 import MainLayout from './hoc/mainLayout';
 import Home from './components/home';
@@ -6,15 +10,35 @@ import Header from './components/navigation/header'
 import Auth from './components/auth';
 
 const Router = () =>{
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const users = useSelector(state=>state.users);
+
+  useEffect(()=>{
+    dispatch(isAuth())
+  },[])
+
+  useEffect(()=>{
+    if(users.auth !== null){
+      setLoading(false)
+    }
+  },[users])
+
   return(
     <BrowserRouter>
-      <Header/>
-      <MainLayout>
-        <Routes>
-          <Route path='/auth' element={<Auth/>}/>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
-      </MainLayout>
+    { loading ?
+      <Loader/>
+    :
+      <>
+        <Header/>
+        <MainLayout>
+          <Routes>
+            <Route path='/auth' element={<Auth/>}/>
+            <Route path="/" element={<Home/>}/>
+          </Routes>
+        </MainLayout>
+      </>
+    } 
     </BrowserRouter>
   )
 }
