20ce9585d8fec72f2f1dba82549f87de1a41e954
Coding rev <fernando.lobo.prez@gmail.com>
2025-04-24 00:54:54 -0300

    55-Admin layout component
    55-Admin layout component

--- frontend/src/components/dashboard/main.jsx
+++ frontend/src/components/dashboard/main.jsx
@@ -1,6 +1,9 @@
+import { AdminTitle } from "../../utils/tools";
+
 const DashboardMain = () => {
     return(
         <>
+            <AdminTitle title="Dashboard"/>
             Dashboard main
         </>
     )
