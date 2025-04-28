450d013f35a53937bfb4106abdc2c9cba42773fe
Coding rev <fernando.lobo.prez@gmail.com>
2025-04-28 15:52:22 -0300

    76-Article by Id score card
    76-Article by Id score card

--- frontend/src/utils/scoreCard.jsx
+++ frontend/src/utils/scoreCard.jsx
@@ -1,8 +1,61 @@
+import {
+    List,
+    ListItem,
+    ListItemText,
+    ListItemAvatar,
+    Avatar,
+    Divider,
+    Chip
+} from '@mui/material';
+import MovieIcon from '@mui/icons-material/Movie'
+import PersonIcon from '@mui/icons-material/Person'
+import StarIcon from '@mui/icons-material/Star'
+
 
 const ScoreCard = ({current}) => {
     return(
         <>
-            score card
+        <hr/>
+        <List className='scorecard'>
+            <ListItem>
+                <ListItemAvatar>
+                    <Avatar><StarIcon/></Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                    primary="Our score"
+                    secondary={current.score}
+                    className='rating'
+                />
+            </ListItem>
+            <Divider variant='inser' component="li"/>
+            <ListItem>
+                <ListItemAvatar>
+                    <Avatar><PersonIcon/></Avatar>
+                </ListItemAvatar>
+                <div>
+                    { current.actors.map((item,index)=>(
+                        <Chip
+                            key={`${item+index}`}
+                            item={item}
+                            label={item}
+                            clickable
+                            color="primary"
+                            className='chip'
+                        />
+                    ))}
+                </div>
+            </ListItem>
+            <Divider variant='inser' component="li"/>
+            <ListItem>
+                <ListItemAvatar>
+                    <Avatar><MovieIcon/></Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                    primary="Director"
+                    secondary={current.director}
+                />
+            </ListItem>
+        </List>
         </>
     )
 }
